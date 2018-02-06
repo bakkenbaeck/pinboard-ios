2ae618149047d3bec1cfa3b3a5be1395f0ae4840
Igor Ranieri Elland <igor@elland.me>
2018-02-06 10:57:44 +0100

    Init.

---
+++ pinboard-ios/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  pinboard-ios
+//
+//  Created by Igor Ranieri on 06.02.18.
+//  Copyright © 2018 Bakken & Bæck. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ pinboard-iosTests/pinboard_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  pinboard_iosTests.swift
+//  pinboard-iosTests
+//
+//  Created by Igor Ranieri on 06.02.18.
+//  Copyright © 2018 Bakken & Bæck. All rights reserved.
+//
+
+import XCTest
+@testable import pinboard_ios
+
+class pinboard_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
